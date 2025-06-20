56f601cd0835fb85143a4ba6ab0f542df1094a67
Shreyan Das <shreyandas57@gmail.com>
2025-06-20 14:54:45 +0530

    week1 complete

---
+++ WEEK1/excercise1/TempApp/exercise1.cs
@@ -0,0 +1,29 @@
+using System;
+
+// Singleton Class
+class Singleton
+{
+    // Static instance initialized at the time of class loading
+    public static Singleton Instance = new Singleton();
+
+    // Private constructor to prevent external instantiation
+    private Singleton()
+    {
+        Console.WriteLine("Singleton Created.");
+    }
+
+    // Public method
+    public void HelloWorld()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
+// Main Class
+class Exercise1
+{
+    static void Main(string[] args)
+    {
+        Singleton.Instance.HelloWorld();
+    }
+}
