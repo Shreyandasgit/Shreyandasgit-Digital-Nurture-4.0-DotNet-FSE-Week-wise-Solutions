4a3a3342efe742fc9970322de48dadc329d4ba54
Shreyan Das <shreyandas57@gmail.com>
2025-06-20 15:23:16 +0530

    week1

--- WEEK1/excercise1/TempApp/obj/Debug/net9.0/TempApp.AssemblyInfo.cs
+++ WEEK1/excercise1/TempApp/obj/Debug/net9.0/TempApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TempApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdbdca8a819bd938946564c2bc51709afb5a1e8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95c1863d1f9da53a69b3da917d6bba56b8d78e09")]
 [assembly: System.Reflection.AssemblyProductAttribute("TempApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TempApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
